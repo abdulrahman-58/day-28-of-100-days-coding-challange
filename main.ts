55b25015cce3acbd1740502eac8beed67122e126
abdulrahman <abdulrahmanazam58@gmail.com>
2024-06-14 04:40:49 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,30 @@
+// Assalamualaikum @everyone 👋
+
+// 🚀 Day 28 Challenge: Start Coding! 🚀
+
+// Question 82: Find the Length of a String: Write a function that takes a string as an argument and returns the number of characters in the string.
+function stringLength(str: string): number {
+  return str.length;
+}
+console.log(stringLength("Pakistan"));
+console.log(`\n`);
+
+// Question 83: Converting to Upper and Lower Case: Create a function that takes a string, converts it to uppercase, then to lowercase, and logs both.
+function convertCase(str: string) {
+  let upperStr = str.toUpperCase();
+  let lowerStr = str.toLowerCase();
+  console.log("Uppercase: ", upperStr);
+  console.log("Lowercase: ", lowerStr);
+}
+convertCase("Hey Dear!");
+console.log(`\n`);
+
+// Question 84: Replacing Text in a String: Write a function that takes a sentence and replaces all instances of the word "JavaScript" with "TypeScript".
+function replaceJavaScriptWithTypeScript(sentence: string): string {
+  return sentence.replace(/JavaScript/g, "TypeScript");
+}
+console.log(
+  replaceJavaScriptWithTypeScript(
+    "I love JavaScript and I am coding in in JavaScript."
+  )
+);
